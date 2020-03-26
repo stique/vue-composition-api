fb50d0b7ae9f82cae06e765d422ef31c147c1359
Kirill Rebrikov <stique@reg.ru>
2020-03-26 17:48:51 +0400

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,142 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <p>{{ vTitle }}</p>
+
+    <div class="my-3 text-primary">
+      <p>{{ append }}</p>
+    </div>
+
+    <div class="form-group d-flex">
+      <input v-model="todo" type="text" class="form-control" />
+      <button class="btn btn-primary" @click="add">Add</button>
+    </div>
+    <p>[TodoLength: {{ todoLength }}, Items quantity: {{ itemsQuantity }}]</p>
+
+    <div class="list-group">
+      <div
+        class="list-group-item d-flex justify-content-between"
+        v-for="(item,index) in items"
+        :key="index"
+      >
+        <span>{{ item }}</span>
+        <button class="close" @click="remove(index)">
+          <span>&times;</span>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref, computed, watch, onBeforeMount, onMounted } from '@vue/composition-api';
+
+// функция которая не возвращает ничего в шаблон
+function useCount() {
+  const count = ref(1);
+  const double = computed({
+    get: () => count.value * 2,
+    set: val => { count.value = val - 1 },
+  });
+
+  console.log(count.value);
+  console.log(double.value);
+}
+
+// функция использующая на входе props
+function useTitle(props) {
+  const vTitle = computed(() => `-${props.title}-`);
+
+  return {
+    vTitle
+  };
+}
+
+// функция использующая на входе ref заданный в setup()
+function useTodo(todo) {
+  const todoLength = ref(0);
+
+  watch(
+    () => todo.value.length,
+    length => {
+      todoLength.value = length;
+    },
+    {
+      lazy: false,
+    }
+  )
+
+  return {
+    todoLength
+  };
+}
+
+// функция использующая на входе ref заданный в setup()
+function useItems(todo) {
+  const items = ref(['This', 'is']);
+  const itemsQuantity = computed(() => items.value.length);
+  const append = ref('');
+
+  watch(
+    // getter
+    () => items.value,
+    // callback
+    items => {
+      append.value = '';
+      items.forEach( item => {
+        append.value += item + ' ';
+      });
+    },
+    // watch options
+    {
+      lazy: false,
+    },
+  )
+
+  const add = () => {
+    if (todo.value) {
+      items.value.push(todo.value);
+      todo.value = '';
+    }
+  };
+
+  const remove = index => {
+    items.value.splice(index, 1);
+  };
+
+  return {
+    items,
+    itemsQuantity,
+    append,
+    add,
+    remove,
+  };
+}
+
 export default {
-  name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    title: String,
+    initInput: String,
+  },
+
+  setup(props) {
+    const todo = ref(props.initInput);
+
+    onBeforeMount(() => {
+      console.log('V3 beforeMount!');
+    })
+
+    onMounted(() => {
+      console.log('V3 mounted!');
+    })
+
+    return {
+      todo,
+      ...useCount(),
+      ...useTitle(props),
+      ...useItems(todo),
+      ...useTodo(todo),
+    };
+  },
 }
 </script>
 
